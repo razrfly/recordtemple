05eb943893c2481426ab5b946cf0ac0bd765851a
Holden Thomas <holden@Holden-Thomass-MacBook-Pro.local>
2009-12-11 10:22:53 -0600

    lots, jrail, fixes, stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,8 @@ config.cache_classes = true
 
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = true  #remove this!!!
-config.action_controller.perform_caching             = true
+config.action_controller.perform_caching             = false
+config.action_view.cache_template_loading            = true
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
