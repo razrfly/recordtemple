23b6ea1f29212ed3bdfb880232da7b058371c290
Holden Thomas <holden@Holden-Thomass-MacBook-Pro.local>
2009-12-11 17:14:22 -0600

    mp3 info

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,8 @@ Rails::Initializer.run do |config|
   config.gem "paperclip"
   config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem "jrails"
-  #config.gem "ruby-mp3info", :lib => "ruby-mp3info"
+  #config.gem "iconv"
+  config.gem "ruby-mp3info"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
