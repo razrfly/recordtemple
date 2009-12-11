73c952520160230570a69189e18f046acebd17ad
Holden Thomas <holden@Holden-Thomass-MacBook-Pro.local>
2009-12-11 13:29:11 -0600

    no more prawn

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,6 @@ Rails::Initializer.run do |config|
   config.gem "paperclip"
   config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem "jrails"
-  #config.gem "prawn"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
