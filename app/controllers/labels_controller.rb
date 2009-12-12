a460f3f9569e8f67d9ddd5ef034b13b9e7efa8e3
Holden Thomas <holden@Holden-Thomass-MacBook-Pro.local>
2009-12-12 16:55:01 -0600

    image labels

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,6 +1,6 @@
 class LabelsController < ApplicationController
   def index
-    @labels = Record.find(:all, :select => 'DISTINCT(label), MIN(records.id)', :joins => [ :price, :photos ], :group => :label)
+    @labels = Record.find(:all, :select => 'DISTINCT(label), MIN(records.id) as ID', :joins => [ :price, :photos ], :group => :label)
   end
 
 end
