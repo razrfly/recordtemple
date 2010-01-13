1e975ba0f6d1cbd4b00901b260a60ec38311a51e
holden <holden@owsziki.local>
2010-01-13 18:04:27 +0100

    small

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,7 +1,7 @@
 class Notifier < ActionMailer::Base
   
   def recommendation(recommendation, record_url)
-    subject    'You have a shared record to view'
+    subject    'You have some hot vinyl and its burning a hole in your inbox!'
     recipients recommendation.email
     from       "#{recommendation.record.user.login}@recordtemple.com"
     body       :recommendation => recommendation, :record_url => record_url
