ff92eb25c6758e49654fd3ac266ba91db967a30a
holden <holden@owsziki.local>
2010-01-17 17:41:12 +0100

    heroku dynos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '5e49bedcc578570fc00e25637e1d7794'
   
+  helper_method :backlog, :dynos
+  
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
