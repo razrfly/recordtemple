afd8b3cc23db06c5bd1e6f6fd13ad5ff26287749
holden <holden@owsziki.local>
2010-01-17 19:27:25 +0100

    heroku dynos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '5e49bedcc578570fc00e25637e1d7794'
   
-  helper_method :backlog, :dynos
+  helper_method :backlog, :dynos, :new_dyno, :saveme
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
@@ -17,11 +17,22 @@ class ApplicationController < ActionController::Base
   private
   
   def backlog
-    request.env["HTTP_X_HEROKU_QUEUE_DEPTH"]
+    request.env["HTTP_X_HEROKU_QUEUE_DEPTH"].to_i
   end
   
   def dynos
     request.env["HTTP_X_HEROKU_DYNOS_IN_USE"]
+  end
+  
+  def new_dyno
+    @client = Heroku::Client.new('holden.thomas@gmail.com','sawyer')
+    @client.set_dynos('recordapp', 2)
+  end
+  
+  def saveme
+    if backlog > 10
+      new_dyno
+    end
   end
   
   
