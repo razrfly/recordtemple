dd280b4800d72cdedf47ca0aa0e09006815ba8ab
holden <holden@owsziki.local>
2010-02-07 15:30:03 +0100

    new labels

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,6 +1,9 @@
 class LabelsController < ApplicationController
   def index
-    @labels = Record.find(:all, :select => 'DISTINCT(label), MIN(records.id) as ID', :joins => [ :price, :photos ], :group => :label)
+    @labels = Record.find(:all, 
+                          :select => 'DISTINCT(label), MIN(records.id) as ID', 
+                          :joins => [ :price, :photos ], 
+                          :group => :label).paginate :per_page => 49, :page => params[:page]
   end
 
 end
