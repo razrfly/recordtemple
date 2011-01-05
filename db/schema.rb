cad96b7f4bbec33ce8fc3e172ac8f807f3cd37d9
Holden Thomas <holden.thomas@gmail.com>
2011-01-05 20:53:35 +0100

    mega

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110105190421) do
+ActiveRecord::Schema.define(:version => 20110105194902) do
 
   create_table "artists", :force => true do |t|
     t.string   "name"
