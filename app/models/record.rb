9cb5337ecf5a49a218904533db987f3bdb4859d8
Holden Thomas <holden.thomas@gmail.com>
2011-01-06 14:56:55 +0100

    secure mp3s

--- app/models/record.rb
+++ app/models/record.rb
@@ -18,25 +18,6 @@ class Record < ActiveRecord::Base
   acts_as_tree :foreign_key => "price_id"
   accepts_nested_attributes_for :photos, :songs
   
-  
-  #def self.search(search, page, user)
-  #  unless search.nil? then search = search.downcase end
-  #  paginate :per_page => 20, :page => page,
-  #                            :joins => :price,
-  #                            :conditions => [ 'LOWER(artist) like ? and user_id = ?', "%#{search}%", user.id ],
-  #                            :order => 'updated_at DESC'
-  #end
-  
-  #def self.values(search, user)
-  #  Record.sum(:value, :joins => "left outer join prices on prices.id = records.price_id",
-  #                          :conditions => ['artist like ? and username = ?', "%#{search}%", user])
-  #end
-  
-  #def self.osbourne(search, user)
-  #  Record.sum(:pricehigh, :joins => "left outer join prices on prices.id = records.price_id",
-  #             :conditions => ['artist like ? and username = ?', "%#{search}%", user])
-  #end
-  
   def cyberguide
     if condition <= 2
       price.bubbles.last.high
@@ -65,47 +46,5 @@ class Record < ActiveRecord::Base
   def get_condition
     [["Mint", 1], ["Near Mint", 2], ["Very Good ++", 3], ["Very Good +", 4], ["Very Good", 5], ["Good", 6], ["Poor", 7]]
   end
-  
-  #def mugshot_attributes=(mugshot_attributes)
-  #  mugshot_attributes.each do |attributes|
-  #    mugshots.build(attributes)
-  #  end
-  #end
-  #
-  #def self.chart_raw
-  #  Record.find_by_sql("SELECT Year(created_at) as year, DATE_FORMAT(created_at, '%M') as month, COUNT(id) as num 
-  #                      FROM records WHERE created_at BETWEEN DATE_ADD(CURDATE(), INTERVAL -6 MONTH) AND CURDATE()
-  #                      GROUP BY year, month")
-  #end
-  #
-  #def self.chart_months
-  #  chart = Record.chart_raw
-  #  months = ""
-  #  
-  #  chart.each do |item|
-  #    months += "#{item.month}|"
-  #  end
-  #  
-  #  return months.chop
-  #  
-  #end
-  #
-  #def self.chart_months_data
-  #  chart = Record.chart_raw
-  #  data = ""
-  #  total = 0
-  #  
-  #  chart.each do |item|
-  #    total += item.num.to_i
-  #  end    
-  #  
-  #  chart.each do |item|
-  #    data += "#{((item.num.to_f/total)*100).to_s},"
-  #  end
-  #  
-  #  return data.chop
-  #  #return total
-  #  
-  #end
     
 end
