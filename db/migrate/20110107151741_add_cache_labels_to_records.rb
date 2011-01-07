07e6424d50da36d57d51a7db0b17f98738400edd
Holden Thomas <holden.thomas@gmail.com>
2011-01-07 17:03:51 +0100

    fix greg errors

--- db/migrate/20110107151741_add_cache_labels_to_records.rb
+++ db/migrate/20110107151741_add_cache_labels_to_records.rb
@@ -1,9 +1,9 @@
 class AddCacheLabelsToRecords < ActiveRecord::Migration
   def self.up
     add_column :records, :cached_label, :string
-    Record.all.each do |r|
-      r.update_attribute :cached_label, r.label.name
-    end
+    #Record.all.each do |r|
+    #  r.update_attribute :cached_label, r.label.name
+    #end
   end
 
   def self.down
