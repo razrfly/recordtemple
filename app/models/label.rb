7c67656944fcee94ba53f5144e07ce715fa96f87
Holden Thomas <holden.thomas@gmail.com>
2011-01-07 13:08:35 +0100

    stuff

--- app/models/label.rb
+++ app/models/label.rb
@@ -3,6 +3,12 @@ class Label < ActiveRecord::Base
   has_many :prices
   has_many :artists, :through => :records, :uniq => true
   
+  has_friendly_id :name, :use_slug => true,
+      # remove accents and other diacritics from Latin characters
+      :approximate_ascii => true,
+      # don't use slugs larger than 50 bytes
+      :max_length => 50
+  
   index do
     name
   end
