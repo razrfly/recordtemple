cdd9e7ca8424ddb2ec9f40d74e5eea7fc610d0ba
Holden Thomas <holden.thomas@gmail.com>
2011-01-07 12:45:01 +0100

    freebase

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -10,23 +10,21 @@ class Artist < ActiveRecord::Base
   
   validates_presence_of :name
   
+  def description
+      Freebase.find(freebase_id).description unless freebase_id.blank?
+  end
+  
+  def thumbnail
+      Freebase.find(freebase_id).thumbnail unless freebase_id.blank?
+  end
+  
   def self.find_freebase(artist)
-    
     search = Ken.session.mqlread([{
       :type => "/music/artist",
       :id => nil,
       :limit => 1,
       :name => artist
     }])
-    
   end
   
-  def uncomma
-    if name.match(',')
-      new_name = name.split(',')
-      "#{new_name.last.lstrip} #{new_name.first}" 
-    else
-      name
-    end
-  end
 end
--- app/models/label.rb
+++ app/models/label.rb
@@ -9,25 +9,21 @@ class Label < ActiveRecord::Base
   
   validates_presence_of :name
   
+  def description
+      Freebase.find(freebase_id).description unless freebase_id.blank?
+  end
   
-  def self.find_freebase(label)
-    
+  def thumbnail
+      Freebase.find(freebase_id).thumbnail unless freebase_id.blank?
+  end
+  
+  def self.find_freebase(label)  
     search = Ken.session.mqlread([{
       :type => "/music/record_label",
       :id => nil,
       :limit => 1,
       :"name~=" => label
     }])
-    
   end
   
-  #def self.freebase(id)
-  #  
-  #  search = Ken.session.mqlread([{
-  #    :id => id,
-  #    :name => nil,
-  #    :"/common/topic/image" : [{ "id" : null, "optional" : true, "limit" : 3 }]
-  #  }])
-  #  
-  #end
 end
