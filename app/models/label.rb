438f5b21cce590eea7d7fe7dd941220bbc07053f
Holden Thomas <holden.thomas@gmail.com>
2011-01-09 17:33:12 +0100

    sdfsd

--- app/models/label.rb
+++ app/models/label.rb
@@ -3,11 +3,20 @@ class Label < ActiveRecord::Base
   has_many :prices
   has_many :artists, :through => :records, :uniq => true
   
-  has_friendly_id :name, :use_slug => true,
+  has_friendly_id :name_unless_bad, :use_slug => true,
       # remove accents and other diacritics from Latin characters
       :approximate_ascii => true,
       # don't use slugs larger than 50 bytes
       :max_length => 50
+      
+  def name_unless_bad
+    reserved_words = [ "index", "show", "create", "destroy", "delete", "new", "update" ]
+    unless reserved_words.include?(name.downcase)
+      name
+    else
+      "#{name}-the-artist"
+    end
+  end
   
   index do
     name
