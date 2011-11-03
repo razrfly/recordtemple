1b0ae55f9f888f405fc97d6b293219db9dd72d08
Holden Thomas <holden.thomas@gmail.com>
2011-11-03 01:56:49 +0100

    wee

--- app/admin/record_formats.rb
+++ app/admin/record_formats.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register RecordFormat do
   index do
     id_column
     column :name
+    column :record_type
     #column('count'){ |record_format| record_format.prices.size.to_i }
     #column(){ |record_format| link_to 'Jump', admin_prices_path('q[record_format_id_eq=31]') }
     default_actions
