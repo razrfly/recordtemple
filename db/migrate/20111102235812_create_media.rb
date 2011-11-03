6c6bcaf080f56dbfa2b44ed4b516b19cbe17cf04
Holden Thomas <holden.thomas@gmail.com>
2011-11-03 01:19:15 +0100

    change media

---
+++ db/migrate/20111102235812_create_media.rb
@@ -0,0 +1,15 @@
+class CreateMedia < ActiveRecord::Migration
+  def self.up
+    create_table :media do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    add_column :record_formats, :media_id, :integer
+  end
+
+  def self.down
+    remove_column :record_formats, :media_id
+    drop_table :media
+  end
+end
