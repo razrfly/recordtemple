54a769ba171c020ac30443d0f59f22b995658bcf
Holden Thomas <holden.thomas@gmail.com>
2012-04-28 20:56:17 +0200

    slider

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,13 @@
+$("#carousel-gallery").touchCarousel({				
+	itemsPerPage: 1,				
+	scrollbar: true,
+	scrollbarAutoHide: true,
+	scrollbarTheme: "dark",				
+	pagingNav: false,
+	scrollToLast: true,
+	useWebkit3d: true,				
+	loopItems: false,
+	
+	snapToItems: true,
+	autoplay:true
+});
