7cb68e8f917b9abd3038c5082f6e05787845fcaf
Holden Thomas <holden.thomas@gmail.com>
2012-10-27 13:16:02 +0200

    deletions

--- app/controllers/statistics_controller.rb
+++
@@ -1,8 +0,0 @@
-class StatisticsController < ApplicationController
-  def index
-    @genres = Record.select("DISTINCT genre_id, COUNT(*), SUM(value)").group("genre_id").order("count DESC").limit(8)
-    @genre_list = Genre.limit(5)
-    @genre_value = Record.limit(10).order("VALUE DESC")
-  end
-
-end
