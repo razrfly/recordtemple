b494804b87bbb281ba89802fc952f7e6160f8bef
Szymon Madeja <smadeja1@gmail.com>
2014-07-03 10:33:10 +0200

    Install Webarch deps

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -1,32 +1,25 @@
-//
 //= require jquery
 //= require jquery_ujs
 // require turbolinks
 
-// require ./webarch/plugins/jquery-1.8.3.min
 //= require ./webarch/plugins/jquery-ui-1.10.1.custom.min
-
-// require ./webarch/plugins/bootstrap.min
 //= require bootstrap
 
 //= require ./webarch/plugins/breakpoints
-//= require ./webarch/plugins/jquery.unveil.min
+//= require unveil
 //= require ./webarch/plugins/jqueryblockui
-//= require ./webarch/plugins/jquery.sidr.min
-//= require ./webarch/plugins/jquery.slimscroll.min
+//= require sidr
+//= require jquery.slimscroll/jquery.slimscroll
 //= require ./webarch/plugins/jquery.animateNumbers
-//= require ./webarch/plugins/select2.min
-//= require ./webarch/plugins/jquery.dataTables.min
+//= require select2
+
+//= require dataTables/jquery.dataTables
 //= require ./webarch/plugins/dataTables.tableTools.min
-//= require ./webarch/plugins/datatables.responsive
-//= require ./webarch/plugins/lodash.min
-//
+//= require lodash
+//= require dataTables/extras/dataTables.responsive
+
 //= require ./webarch/core
-//= require ./webarch/chat
-//= require ./webarch/demo
-//
+
 //= require ./webarch/datatable_magic
-//
 //= require ./bootstrap_tools
 //= require ./artists.js.coffee
-//
