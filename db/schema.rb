ff7665baf6e5c132c43cd62160878055dabefe21
Szymon Madeja <smadeja1@gmail.com>
2014-07-07 19:35:18 +0200

    Get devise to work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625230210) do
+ActiveRecord::Schema.define(version: 20140707162328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -211,6 +211,7 @@ ActiveRecord::Schema.define(version: 20140625230210) do
     t.string   "nickname"
     t.string   "fname"
     t.string   "lname"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
