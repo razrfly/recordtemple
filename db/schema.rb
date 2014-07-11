dd6c4e6585883900e1e4bb94be4193abccbb037a
Szymon Madeja <smadeja1@gmail.com>
2014-07-11 11:51:01 +0200

    Add genres

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710110008) do
+ActiveRecord::Schema.define(version: 20140711092027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,10 +56,10 @@ ActiveRecord::Schema.define(version: 20140710110008) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "cached_slug"
+    t.string   "slug"
   end
 
-  add_index "genres", ["cached_slug"], name: "index_genres_on_cached_slug", unique: true, using: :btree
+  add_index "genres", ["slug"], name: "index_genres_on_slug", unique: true, using: :btree
 
   create_table "labels", force: true do |t|
     t.string   "name"
