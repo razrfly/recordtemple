2b1736a14e81c0bfc005495b91eb32b5e2e5d694
Szymon Madeja <smadeja1@gmail.com>
2014-12-29 21:19:32 +0100

    Remove record listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229185851) do
+ActiveRecord::Schema.define(version: 20141229201643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,14 +111,6 @@ ActiveRecord::Schema.define(version: 20141229185851) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "record_type_id"
-  end
-
-  create_table "record_listings", force: true do |t|
-    t.integer  "record_id"
-    t.string   "external_id"
-    t.string   "listing_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "record_types", force: true do |t|
