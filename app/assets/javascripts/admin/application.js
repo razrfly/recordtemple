a18559b8ea556129b4fad952ed5fb5a33a74b408
Szymon Madeja <smadeja1@gmail.com>
2015-01-05 15:19:15 +0100

    Set up pages
    And remove old junk.

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -2,26 +2,7 @@
 //= require jquery_ujs
 // require turbolinks
 
-//= require ./webarch/plugins/jquery-ui-1.10.1.custom.min
-//= require bootstrap
-
-//= require ./webarch/plugins/breakpoints
-//= require unveil
-//= require ./webarch/plugins/jqueryblockui
-//= require sidr
-//= require jquery.slimscroll/jquery.slimscroll
-//= require ./webarch/plugins/jquery.animateNumbers
-//= require select2
-
-//= require dataTables/jquery.dataTables
-//= require ./webarch/plugins/dataTables.tableTools.min
-//= require lodash
-//= require dataTables/extras/dataTables.responsive
-
-//= require ./webarch/core
-
-//= require ./webarch/datatable_magic
-//= require ./bootstrap_tools
+//= require pages-rails
 
 //= require ./artists
 //= require ./labels
