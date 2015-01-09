eea32d688fbda44690285d242c862ba7c5f06273
Szymon <szymon.chorobski@gmail.com>
2015-01-09 14:11:52 +0100

    small fixes

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,13 @@
 class Song < ActiveRecord::Base
+  after_save :create_metadata#, :create_panda
+  #after_destroy :remove_panda
+
   belongs_to :record
-  
+
   Paperclip.interpolates :record_id  do |attachment, style|
     attachment.instance.record_id
   end
-  
+
   has_attached_file :mp3,
     #:style => { :snippet => 'k128' },
     :storage => 's3',
@@ -15,35 +18,31 @@ class Song < ActiveRecord::Base
     :path => "music/:record_id/:style/:basename.:extension",
     :s3_permissions => 'authenticated-read',
     :s3_protocol => 'http'
-    
-  after_save :create_metadata#, :create_panda
-  #after_destroy :remove_panda
-    
+
+
+  #validates_attachment_content_type :mp3, :content_type => [ 'application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3' ], :message => "requires a valid mp3 type"
   validates_attachment_presence :mp3
-    #validates_attachment_content_type :mp3, :content_type => [ 'application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3' ], :message => "requires a valid mp3 type"
   validates_attachment_size :mp3, :less_than => 10.megabytes
-    
   validate :must_have_valid_artist_tag
-  
+
   def authenticated_url
     #AWS::S3::S3Object.url_for(mp3.path(mp3.default_style), mp3.bucket_name, :expires_in => 10.minutes, :use_ssl => mp3.s3_protocol == 'https').html_safe
     #options.reverse_merge! :expires_in => 10.minutes, :use_ssl => true
     #AWS::S3::S3Object.url_for(mp3.path, mp3.options[:bucket], options).html_safe
     AWS::S3::S3Object.url_for(mp3.path, mp3.bucket_name, :expires_in => 10.minutes, :use_ssl => true).html_safe
   end
-  
+
   def panda_url
     panda = Panda::Video.find(panda_id)
-    unless panda.fail?
-      panda.encodings.first.url
-    else
-      #panda.delete
-      #recreate_panda
-    end
+    panda.encodings.first.url unless panda.fail?
+    # else
+    #   #panda.delete
+    #   #recreate_panda
+    # end
   end
 
-    protected
-    
+  protected
+
     def must_have_valid_artist_tag
       Mp3Info.open(mp3.to_file.path) do |mp3info|
         errors.add(:mp3, 'must not be a Michael Bolton song (what are you thinking?!)') if mp3info.tag.artist == 'Michael Bolton'
@@ -51,27 +50,24 @@ class Song < ActiveRecord::Base
     rescue Mp3InfoError => e
       errors.add(:mp3, "unable to process file (#{e.message})")
     end
-    
+
     def create_panda
       #video = Panda::Video.create(:source_url => mp3.url.gsub(/[?]\d*/,''), :profiles => "f4475446032025d7216226ad8987f8e9", :path_format => "music/#{record.id}/snippet/#{mp3_file_name.gsub('.mp3','')}")
       panda = Panda::Video.create(:source_url => authenticated_url, :profiles => "f4475446032025d7216226ad8987f8e9", :path_format => "#{record.artist.id}/#{record.user_id}-#{record_id}-#{id}")
       self.panda_id = panda.id
       self.save
     end
-    
+
     def recreate_panda
       panda = Panda::Video.create(:source_url => mp3.authenticated_url, :profiles => "f4475446032025d7216226ad8987f8e9", :path_format => "#{mp3.record.artist.id}/#{mp3.record.user_id}-#{mp3.record_id}-#{mp3.id}")
       self.panda_id = panda.id
       self.save
     end
-    
+
     def remove_panda
-      unless panda_id.blank?
-        panda = Panda::Video.find(panda_id)
-        panda.delete
-      end
+      Panda::Video.find(panda_id).delete unless panda_id.blank?
     end
-    
+
     def create_metadata
       Mp3Info.open(mp3.to_file.path) do |mp3info|
         if self.title.blank?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :records
+  has_many :prices
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  # Setup accessible (or protected) attributes for your model
-  # attr_accessible :email, :password, :password_confirmation, :remember_me, :nickname, :fname, :lname
-  
-  has_many :records
-  has_many :prices
-  
-  def admin?
-    if email == "greg2man@gmail.com"
-      true
-    else
-      false
-    end
-  end
-  
-  def role?(role)
-    if email.blank?
-      false
-    else
-      true
-    end
-  end
-  
 end
