e9b092455a531608f78d76219f39441968bf5f13
Szymon <szymon.chorobski@gmail.com>
2015-02-23 13:18:30 +0100

    user scoping for filters

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -5,24 +5,36 @@ class Genre < ActiveRecord::Base
   has_many :artists, -> { uniq }, :through => :records
   has_many :labels, -> { uniq }, :through => :records
 
-  # deprecated in favour of :with_records
-  scope :not_empty, -> { all.map{|rf| rf if rf.records.size>0 }.compact }
-
-  scope :with_records, -> do
-    joins('LEFT OUTER JOIN records ON genres.id = records.record_format_id').group('genres.id')
-      .having('count(records.id) > 0')
-  end
-
-  scope :without_records, -> do
-    joins('LEFT OUTER JOIN records ON genres.id = records.record_format_id').group('genres.id')
-      .having('count(records.id) = 0')
-  end
-
   friendly_id :name, :use => [:slugged, :finders]
-
   validates_presence_of :name
 
-  def name_with_count
-    "#{name} (#{records.size.to_s})"
+  scope :with_records, lambda { |user=nil|
+    if user.nil?
+      joins('INNER JOIN records ON genres.id = records.genre_id')
+        .group('genres.id')
+        .having('count(records.id) > 0')
+    else
+      joins('INNER JOIN records ON genres.id = records.genre_id')
+        .where('records.user_id = ?', user.id)
+        .group('genres.id')
+        .having('count(records.id) > 0')
+    end
+  }
+
+  scope :without_records, lambda { |user=nil|
+    if user.nil?
+      joins('LEFT OUTER JOIN records ON genres.id = records.genre_id')
+        .group('genres.id')
+        .having('count(records.id) = 0')
+    else
+      joins('LEFT OUTER JOIN records ON genres.id = records.genre_id')
+        .where('records.user_id = ?', user.id)
+        .group('genres.id')
+        .having('count(records.id) = 0')
+    end
+  }
+
+  def name_with_records_count user=nil
+    "#{name} (#{(user.nil? ? records : user.records.where(genre: self)).size.to_s })"
   end
 end
