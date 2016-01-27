2207e1bda3ca2716f6cf78960c38fa2f0330dfeb
Szymon Madeja <smadeja1@gmail.com>
2016-01-27 17:08:05 +0100

    Create a basic search

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,8 @@
 class RecordsController < ApplicationController
 
   def index
-    @records = (params[:user_id].nil? ? Record.all : Record.where(user: User.find(params[:user_id]))).page(params[:page])
+    @q = Record.ransack(params[:q])
+    @records = @q.result.includes(:artist, :record_format).page(params[:page])
   end
 
   def show
