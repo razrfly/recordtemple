48db0ffa306f953bdf92d1d860ece32ec80debd4
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2016-09-14 12:14:24 +0200

    New navbar + small additional changes
    -> comment out admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Recordapp::Application.routes.draw do
+  # mount Blazer::Engine, at: 'blazer'
 
   root to: "home#index"
   # ransack and selectize
@@ -16,46 +17,48 @@ Recordapp::Application.routes.draw do
     resources :records, only: [:new, :create]
   end
 
-  namespace :admin do
-    mount Blazer::Engine, at: 'blazer'
-    root to: 'home#index'
-    # ransack and selectize
-    put 'artists' => 'artists#index'
-    put 'prices'  => 'prices#index'
-    put 'labels'  => 'labels#index'
-    put 'records' => 'records#index'
-    put 'songs'   => 'songs#index'
-    put 'users'   => 'users#index'
+  # Will be removed
+  # namespace :admin do
 
-    # get ':user_id/pages', to: 'pages#index', as: 'user_pages'
-    resources :pages do
-      post 'sort', to: 'pages#sort', on: :collection
-    end
+  #   root to: 'home#index'
+  #   # ransack and selectize
+  #   put 'artists' => 'artists#index'
+  #   put 'prices'  => 'prices#index'
+  #   put 'labels'  => 'labels#index'
+  #   put 'records' => 'records#index'
+  #   put 'songs'   => 'songs#index'
+  #   put 'users'   => 'users#index'
+
+  #   # get ':user_id/pages', to: 'pages#index', as: 'user_pages'
+  #   resources :pages do
+  #     post 'sort', to: 'pages#sort', on: :collection
+  #   end
 
 
-    resources :genres, :record_formats, :record_types, except: :show
-    resources :pages, only: [:index]
-    resources :users
-    resources :invitations, only: [:new, :create]
-    resources :artists, :prices, :labels
-    resources :records do
-      resources :songs, :photos
-      delete 'unlink_price', on: :member
-    end
-    resources :prices do
-      resources :records
-    end
+  #   resources :genres, :record_formats, :record_types, except: :show
+  #   resources :pages, only: [:index]
+  #   resources :users
+  #   resources :invitations, only: [:new, :create]
+  #   resources :artists, :prices, :labels
+  #   resources :records do
+  #     resources :songs, :photos
+  #     delete 'unlink_price', on: :member
+  #   end
+  #   resources :prices do
+  #     resources :records
+  #   end
 
-    # sticky navigation
-    post 'sticky' => 'admin#sticky'
-  end
+  #   # sticky navigation
+  #   post 'sticky' => 'admin#sticky'
+  # end
 
-  constraints(user_id: /rui|greggie|holden|szymon|simon|twitwilly|steve/) do
-    get ':user_id', to: 'users#show', as: 'user'
-    get ':user_id/records', to: 'records#index', as: 'user_records'
-    get ':user_id/pages', to: 'pages#index', as: 'user_pages'
-    get ':user_id/:id', to: 'pages#show', as: 'user_page'
-  end
-  get 'users', to: 'users#index'
+  #Dont know what is it for
+  # constraints(user_id: /rui|greggie|holden|szymon|simon|twitwilly|steve/) do
+  #   get ':user_id', to: 'users#show', as: 'user'
+  #   get ':user_id/records', to: 'records#index', as: 'user_records'
+  #   get ':user_id/pages', to: 'pages#index', as: 'user_pages'
+  #   get ':user_id/:id', to: 'pages#show', as: 'user_page'
+  # end
+  # get 'users', to: 'users#index'
 
 end
