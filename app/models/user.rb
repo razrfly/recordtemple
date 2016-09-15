c8a73adc6e3e76dae83d6e8581346f796bb4c6d8
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2016-09-15 11:28:10 +0200

    Avatar added to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,23 +8,16 @@ class User < ActiveRecord::Base
   extend FriendlyId
   friendly_id :username, use: [:slugged, :finders]
 
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable,
+    :rememberable, :trackable, :validatable
 
-  def active?
-    invitation_accepted_at.present? | (invitation_accepted_at.nil? & invitation_sent_at.nil?)
-  end
+  attachment :avatar
 
-  def name
-    "#{fname} #{lname}" if fname.present? or lname.present?
-  end
+  # def active?
+  #   invitation_accepted_at.present? | (invitation_accepted_at.nil? & invitation_sent_at.nil?)
+  # end
 
-  # just for testing cancancan
-  def admin?
-    # false
-    true
-  end
-
+  # def name
+  #   "#{fname} #{lname}" if fname.present? or lname.present?
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212102812) do
+ActiveRecord::Schema.define(version: 20160915071753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -240,6 +240,7 @@ ActiveRecord::Schema.define(version: 20160212102812) do
     t.string   "invited_by_type"
     t.integer  "invitations_count",                  default: 0
     t.string   "slug"
+    t.string   "avatar_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
