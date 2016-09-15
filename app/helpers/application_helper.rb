3af3b53a97f266db32a83b0a00265aa6917838bf
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2016-09-15 13:06:41 +0200

    Gravatar helper and small tweaks to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,8 @@ module ApplicationHelper
     end
   end
 
+  def gravatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
