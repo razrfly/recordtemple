5e391d8731d6515f40e43cb8880ba69905a8a5b7
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2016-09-20 12:57:28 +0200

    Index action to artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,15 @@
 class ArtistsController < ApplicationController
 
   def index
+    @q = Artist.ransack(query_params)
+
+    @result = @q.result.
+      includes(:records, :photos).
+      uniq
+
     respond_to do |format|
       format.html do
-        @artists = Artist.active.page(params[:page])
+        @artists = @result.page(params[:page])
       end
 
       format.json do
@@ -17,4 +23,11 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
 
+  private
+
+  def query_params
+    params[:q].try(:reject) do |k, v|
+      ['photos_id_not_null', 'songs_id_not_null'].include?(k) && v == '0'
+    end
+  end
 end
