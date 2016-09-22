299e292b9b234412f6b52f36a1c3769f535028c0
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2016-09-22 09:41:09 +0200

    Remove records counter from artists table

--- db/migrate/20160920125328_add_records_count_to_artists.rb
+++
@@ -1,5 +0,0 @@
-class AddRecordsCountToArtists < ActiveRecord::Migration
-  def change
-    add_column :artists, :records_count, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920125328) do
+ActiveRecord::Schema.define(version: 20160915071753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20160920125328) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
-    t.integer  "records_count"
   end
 
   add_index "artists", ["name"], name: "index_artists_on_name", unique: true, using: :btree
