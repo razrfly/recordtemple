a57b84bb4c099f3df637f47881c8392f79fd9db6
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2016-10-05 09:16:38 +0200

    Ransack scope method to provide search through price range on records

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,10 +1,10 @@
 class Price < ActiveRecord::Base
-  has_many :records#, :dependent => :destroy
+
   belongs_to :record_format
   belongs_to :artist
   belongs_to :label
   belongs_to :user
-  # has_one :record ????
+  has_many :records
 
   delegate :name, to: :artist, prefix: true, allow_nil: true
   delegate :name, to: :label, prefix: true, allow_nil: true
