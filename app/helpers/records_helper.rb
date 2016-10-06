62399a5b46306509d4397de88a85cf28cf9f9a09
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2016-10-06 14:27:09 +0200

    Forgot to remove useless stuff

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,13 +1,2 @@
 module RecordsHelper
-
-  def slider_value_helper search_query
-    if search_query.price_between.present?
-      low, high = search_query.price_between.
-      split(',').map &:to_i
-
-      {'slider-value': [low, high]}
-    else
-      {'slider-value': [0,0]}
-    end
-  end
 end
