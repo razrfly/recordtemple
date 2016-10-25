346e95082efeae0229cb25c2205392b2a2966565
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2016-10-25 11:32:57 +0200

    Record Type index prepared for new record type ajax calls

--- app/models/record_type.rb
+++ app/models/record_type.rb
@@ -1,10 +1,4 @@
 class RecordType < ActiveRecord::Base
   has_many :record_formats
   has_many :records, -> { uniq }, :through => :record_formats
-
-  scope :with_counted_records, -> {
-    joins(:records).
-    select('record_types.name, COUNT(record_format_id) as count_records').
-    group('record_types.name')
-  }
 end
