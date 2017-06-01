031f92241e3e9d81df845c707e2e53ac38e8701c
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-06-01 09:59:01 +0200

    Add simple execution introspection features - could be useful later

--- lib/data_dispatcher/data_dispatcher.rb
+++ lib/data_dispatcher/data_dispatcher.rb
@@ -1,9 +1,14 @@
 class DataDispatcher
+  EXECUTE_MESSAGE = {
+    :load_file => 'Loading file ... This could take a while.'
+  }
+
   def initialize(file_name)
     @file_name = file_name
   end
 
   def execute!
+    set_start_time
     create_file_path
     load_file
   end
@@ -11,12 +16,28 @@ class DataDispatcher
   private
   attr_reader :original_file
 
+  def set_start_time
+    @start_time = Time.now
+  end
+
+  def execute_time
+    @execute_time ||= 0 and @execute_time += (Time.now - @start_time)
+    puts "Done. Execution time: #@execute_time"
+  end
+
+  def execute_message(command)
+    puts EXECUTE_MESSAGE[command]
+  end
+
   def create_file_path
     @file_path = Rails.root.join('lib', 'data_dispatcher', @file_name)
   end
 
   def load_file
+    execute_message(__callee__)
+
     File.exist?(@file_path) && @original_file = open(@file_path)
+    execute_time
   end
 
   class << self
