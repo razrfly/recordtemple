3c9eafea6b0d3fe90a3801404435a0e3276645ad
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-06-01 10:55:01 +0200

    Add +fetch_allowed_paragraphs+ command method to execution stack
    -> Remove all nokogiri nodes which have unnecessary data

--- lib/data_dispatcher/data_dispatcher.rb
+++ lib/data_dispatcher/data_dispatcher.rb
@@ -1,7 +1,8 @@
 class DataDispatcher
   EXECUTE_MESSAGE = {
     :load_file => 'Loading file... This could take a while.',
-    :parse_file_with_nokogiri => 'Parsing file with nokogiri...'
+    :parse_file_with_nokogiri => 'Parsing file with nokogiri...',
+    :fetch_allowed_paragraphs => 'Extracting allowed paragraphs from parsed file.'
   }
 
   def initialize(file_name)
@@ -13,10 +14,11 @@ class DataDispatcher
     create_file_path
     load_file
     parse_file_with_nokogiri
+    fetch_allowed_paragraphs
   end
 
   private
-  attr_reader :original_file, :parsed_file
+  attr_reader :original_file, :parsed_file, :paragraphs
 
   def set_start_time
     @start_time = Time.now
@@ -48,6 +50,16 @@ class DataDispatcher
     @parsed_file = Nokogiri::HTML(original_file) do |config|
       config.options |= Nokogiri::XML::ParseOptions::HUGE |
         Nokogiri::XML::ParseOptions::NOBLANKS
+    end
+    execute_time
+  end
+
+  def fetch_allowed_paragraphs
+    execute_message(__callee__)
+    allowed_paragraphs = %w(p2 p3 p4 p5)
+
+    @paragraphs = Array(parsed_file.css('p')).keep_if do |paragraph|
+      allowed_paragraphs.include? paragraph.attr('class')
     end
     execute_time
   end
