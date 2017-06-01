79d0b09d8f381f7260fe91a29bd91866472bd23c
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-06-01 09:49:38 +0200

    Add +load_file+ command method to execution stack

--- lib/data_dispatcher/data_dispatcher.rb
+++ lib/data_dispatcher/data_dispatcher.rb
@@ -5,12 +5,18 @@ class DataDispatcher
 
   def execute!
     create_file_path
+    load_file
   end
 
   private
+  attr_reader :original_file
 
   def create_file_path
     @file_path = Rails.root.join('lib', 'data_dispatcher', @file_name)
+  end
+
+  def load_file
+    File.exist?(@file_path) && @original_file = open(@file_path)
   end
 
   class << self
