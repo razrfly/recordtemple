20c538b45242be28f6c34de2c54a979abb5bd89f
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-06-01 09:45:18 +0200

    Presumably command pattern will do the job here + create_file_path command

--- lib/data_dispatcher/data_dispatcher.rb
+++ lib/data_dispatcher/data_dispatcher.rb
@@ -3,6 +3,16 @@ class DataDispatcher
     @file_name = file_name
   end
 
+  def execute!
+    create_file_path
+  end
+
+  private
+
+  def create_file_path
+    @file_path = Rails.root.join('lib', 'data_dispatcher', @file_name)
+  end
+
   class << self
     def call(file_name)
       new(file_name).execute!
