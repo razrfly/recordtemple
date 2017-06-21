9e60c4ac1d97e8a808e15c6a3c5dab026429db86
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2017-06-21 16:03:55 +0200

    Little +method_missing+ magic to create counters for parsed data in between all source files

--- lib/data_dispatcher/stats.rb
+++ lib/data_dispatcher/stats.rb
@@ -10,6 +10,15 @@ module Stats
   puts "Missing year end:              #{missing_yearend[:original]}"
   puts "Missing footnote:              #{missing_footnote[:original]}"
   puts "Total original prices:         #{@@original_prices['total']}"
+  puts "\n\n#{'*' * 40}\n\n"
+  puts "\tParsed Data\n"
+  puts "Missing labels:                #{missing_cached_label[:parsed]}"
+  puts "Missing details:               #{missing_detail[:parsed]}"
+  puts "Missing price low:             #{missing_price_low[:parsed]}"
+  puts "Missing price high:            #{missing_price_high[:parsed]}"
+  puts "Missing year begin:            #{missing_yearbegin[:parsed]}"
+  puts "Missing year end:              #{missing_yearend[:parsed]}"
+  puts "Missing footnote:              #{missing_footnote[:parsed]}"
  end
 
  def initialize_counters
@@ -23,5 +32,21 @@ module Stats
     'footnote' => Price.where(footnote: [nil, '']).count,
     'total' => Price.count
   }
+ end
+
+ def method_missing(name, *args)
+  super unless name =~ /^missing_(.*)$/
+
+  original_prices = @@original_prices.send(:[], $1)
+  parsed_prices = DataDispatcher::SOURCE_FILES.inject(0) do |result, file_name|
+    sub_prices = self.class.class_variable_get(:@@prices)[file_name]
+    result + sub_prices.try(:count) { |x| x.send(:[], $1).nil? }.to_i
+  end
+
+  { original: original_prices, parsed: parsed_prices }
+ end
+
+ def respond_to_missing?(name, include_private = false)
+  name =~ /^missing_(.*)$/ || super
  end
 end
