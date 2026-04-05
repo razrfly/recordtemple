395cf8c6a6efeb924c1dca079eb837a8c63ea868
Holden Thomas <holden@razrfly.com>
2026-04-05 10:27:28 +0200

    small

--- app/controllers/api/labels_controller.rb
+++ app/controllers/api/labels_controller.rb
@@ -6,12 +6,15 @@ module Api
       return render json: [] if query.length < 2
 
       labels = Label
-        .where("name ILIKE ?", "%#{ActiveRecord::Base.sanitize_sql_like(query)}%")
+        .left_joins(:records)
+        .where("labels.name ILIKE ?", "%#{ActiveRecord::Base.sanitize_sql_like(query)}%")
+        .group("labels.id")
         .order(:name)
         .limit(10)
+        .select("labels.id, labels.name, COUNT(records.id) AS record_count")
 
       render json: labels.map { |l|
-        { id: l.id, name: l.name }
+        { id: l.id, name: l.name, count: l.record_count }
       }
     end
   end
